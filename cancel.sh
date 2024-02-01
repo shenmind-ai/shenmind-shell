4cc59e0357d048fe2e651aad8d122670dc6e001b
admin <admin@mmdatong.com>
2024-02-01 16:58:27 +0800

    cancel prediction

--- cancel.sh
+++ cancel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SHENMIND_API_TOKEN="b518880c4762276bcf3041b309745929706989962a6b0bdda1a100a4e1ffa4cd"
-predictionId="-k6jDE2uhkb4aaPMIjvwTw=="
+SHENMIND_API_TOKEN="xxxxxxxxxxxxxxxxxxxxx"
+predictionId="S6gDFnzwHiZvLEdyMBE6Qw=="
 
 cancelUrl="https://mmdatong.com/api/protected/prediction/cancelPrediction"
 
